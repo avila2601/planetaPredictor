f7d4aef831d7083b1573374b98bb788dd2fcd020
diego avila <diego2601@gmail.com>
2025-03-27 15:39:29 -0500

    21 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,40 @@
-import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule, RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { LoginComponent } from "./components/login/login.component";
-
+import { filter } from 'rxjs/operators';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, NavBarComponent, LoginComponent],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    NavBarComponent,
+    LoginComponent,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'planeta-predictor';
+  isRegistroRoute = false;
+
+  constructor(private router: Router) {
+    // Suscripción a los eventos de navegación
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      // Actualiza isRegistroRoute solo cuando estamos en la ruta de registro
+      this.isRegistroRoute = event.url === '/registro';
+    });
+  }
+
+  ngOnInit() {
+    // Verifica la ruta inicial
+    const currentUrl = this.router.url;
+    this.isRegistroRoute = currentUrl === '/registro';
+  }
 }
