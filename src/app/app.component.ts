177a7bec4c9ff531b5f6f357509259c7b42fda0c
avila2601 <diegoavila2601@gmail.com>
2025-07-10 23:36:34 -0500

    feat: loginajuste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterModule, RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { LoginComponent } from "./components/login/login.component";
-import { filter } from 'rxjs/operators';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -11,30 +8,11 @@ import { CommonModule } from '@angular/common';
   imports: [
     RouterOutlet,
     RouterModule,
-    NavBarComponent,
-    LoginComponent,
-    CommonModule
+    NavBarComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'planeta-predictor';
-  isRegistroRoute = false;
-
-  constructor(private router: Router) {
-    // Suscripción a los eventos de navegación
-    this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe((event: any) => {
-      // Actualiza isRegistroRoute solo cuando estamos en la ruta de registro
-      this.isRegistroRoute = event.url === '/registro';
-    });
-  }
-
-  ngOnInit() {
-    // Verifica la ruta inicial
-    const currentUrl = this.router.url;
-    this.isRegistroRoute = currentUrl === '/registro';
-  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,6 +11,7 @@ import { RecuperarPasswordComponent } from './components/recuperar-password/recu
 import { CrearPollaComponent } from './components/crear-polla/crearpolla.component';
 import { AdministrarComponent } from './components/administrar/administrar.component';
 import { JoinPollaComponent } from './components/join-polla/join-polla.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent },
@@ -25,6 +26,7 @@ export const routes: Routes = [
   { path: 'perfil', component: PerfilComponent, canActivate: [AuthGuard] },
   { path: 'reglamento', component: InfoGeneralComponent, canActivate: [AuthGuard] },
   { path: 'recuperar-password', component: RecuperarPasswordComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegistroComponent },
   { path: 'modal', component: CrearPollaComponent },
   { path: '**', redirectTo: '' }
